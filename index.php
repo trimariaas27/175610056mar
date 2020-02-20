e12b9bab43db98428aaadac0b06dd62f1f547e7d
trimariaas27 <mariatrias084@gmail.com>
2020-02-20 17:45:36 +0700

    Upload baru

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> MY TRIAL WEB </title>
+	 <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<style>
+  .bg-1 { 
+      background-color: #1abc9c;
+      color: #ffffff;
+  }
+  .bg-2 { 
+      background-color: #474e5d;
+      color: #ffffff;
+  }
+  .bg-3 { 
+      background-color: #BDB76B;
+      color: white;
+  }
+  .bg-4 { 
+      background-color: #000000;
+      color: white;
+  }
+  </style>
+</head>
+
+<body style="width:100%">
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="main.php">My Trial Website</a>
+    </div>
+    <ul class="nav navbar-nav">
+     
+    </ul>
+  </div>
+</nav>
+	<div class="container text-center" style="margin-top:6em">
+	<div class="container-fluid bg-1 text-center">
+  <h3>Who Am I ?</h3>
+  <img src="images/maria.jpg" class="img-circle" alt="Bird" width="40%" height="40%">
+  <h3>My name is Maria Tri Astuti</h3>
+</div>
+
+<div class="container-fluid bg-2 text-center">
+  <h3>What Am I?</h3>
+  <p>I am an amateur programmer and a student of STMIK Akakom who is studying to become a programmer</p>
+</div>
+
+<div class="container-fluid bg-3 text-center">
+  <h3>Where To Find Me?</h3>
+  	<p>If you want to find me, you can meet me at STMIK Akakom or at my boarding house in Janti. If you would like to contact me, please contact my email at mariatrias084@gmail.com </p>
+</div>
+
+	</div>
+<footer class="container-fluid bg-4 text-center" style="margin-top:4em">
+  <p>copyright © 2019 Maria | mariatrias084@gmail.com | Desain Theme Made By <a href="https://www.w3schools.com">www.w3schools.com</a></p> 
+</footer>
+		
+</body>
+
+</html>
+		
